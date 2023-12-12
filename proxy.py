4f57148afb15449d36f1c9f7595ea79e6f7ad3e0
ᴛʜᴇ ᴢᴇᴇᴅ <redcereny@hotmail.com>
2023-12-12 14:25:18 +0300

    Delete proxy.py

--- proxy.py
+++
@@ -1,9 +0,0 @@
-# Setting up Proxies with the requests Library
-import requests
-
-proxy_servers = {
-   'http': 'http://proxy.sample.com:8080',
-   'https': 'http://secureproxy.sample.com:8080',
-}
-
-response = requests.get('sample.abc', proxies=proxy_servers)
